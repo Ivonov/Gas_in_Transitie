450350f4ac9612f7e70780f343c35848a39a33ef
Ivonov <j.reijneveld@gmail.com>
2015-10-26 13:05:47 +0100

    Start opzet
    Dit is het basis opzet voor het project in Unity.
    
    TODO:
    - Classen naam veranderen
    - Parameters toevoegen
    - Formules bepalen en implementeren
    - Imports van UnityEngine toevoegen aan classen

---
+++ Assets/Code/fysica/Formules.cs
@@ -0,0 +1,15 @@
+public class Formules 
+{
+	private type parameterName;
+
+	private ReturnType MethodName(Parameters) 
+	{
+		get{}
+		set{}
+	}
+
+	public ReturnType MethodName(Parameters) 
+	{
+		
+	}
+}
---
+++ Assets/Code/vraag_en_aanbod/CvKetel.cs
@@ -0,0 +1,15 @@
+public class CvKetel 
+{
+	private type parameterName;
+
+	private ReturnType MethodName(Parameters) 
+	{
+		get{}
+		set{}
+	}
+
+	public ReturnType MethodName(Parameters) 
+	{
+		
+	}
+}
